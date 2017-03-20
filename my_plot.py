643ec9b01667684eed3f05fb3b08da46eb639533
Loïc Estève <loic.esteve@ymail.com>
2017-03-20 16:34:53 +0100

    wip

--- my_plot.py
+++ my_plot.py
@@ -9,51 +9,54 @@ import matplotlib.pyplot as plt
 
 filename = sys.argv[1]
 
+sns.set_style('ticks')
 col_names = ['library', 'algo_name', 'build_time', 'best_search_time',
              'best_precision']
 df = pd.read_csv(filename, sep='\t', header=None, names=col_names)
 df['best_queries_per_second'] = 1 / df['best_search_time']
-grid = sns.FacetGrid(df, hue='library', legend_out=False)
+grid = sns.FacetGrid(df, hue='library', legend_out=False,
+                     hue_order=sorted(df.library.unique()),
+                     size=6)
 grid.map(plt.scatter, 'best_precision', 'best_queries_per_second').set(
-    yscale='log').add_legend()
+    yscale='log', xlim=(-0.1, 1.1), ylim=(1, 1e5)).add_legend()
 
-all_algos = sorted(set(df.library), key=str.lower)
+# all_algos = sorted(set(df.library), key=str.lower)
 
-colors = plt.cm.Set1(np.linspace(0, 1, len(all_algos)))
-linestyles = {}
-for i, algo in enumerate(all_algos):
-    linestyles[algo] = (colors[i], ['--', '-.', '-', ':'][i%4], ['+', '<', 'o', 'D', '*', 'x', 's'][i%7])
+# colors = plt.cm.Set1(np.linspace(0, 1, len(all_algos)))
+# linestyles = {}
+# for i, algo in enumerate(all_algos):
+#     linestyles[algo] = (colors[i], ['--', '-.', '-', ':'][i%4], ['+', '<', 'o', 'D', '*', 'x', 's'][i%7])
 
-handles = []
-labels = []
+# handles = []
+# labels = []
 
-plt.figure()
-# plt.figure(figsize=(7, 7))
-for algo in all_algos:
-    this_df = df[df.library == algo]
-    data = this_df.copy()
-    data.sort_values(by='best_queries_per_second', ascending=False, inplace=True) # key=lambda t: t[-2]) # sort by time
-    # ys = data.best_queries_per_second
-    # xs = data.best_precision
-    # ls = data.algo_name
+# plt.figure()
+# # plt.figure(figsize=(7, 7))
+# for algo in all_algos:
+#     this_df = df[df.library == algo]
+#     data = this_df.copy()
+#     data.sort_values(by='best_queries_per_second', ascending=False, inplace=True) # key=lambda t: t[-2]) # sort by time
+#     # ys = data.best_queries_per_second
+#     # xs = data.best_precision
+#     # ls = data.algo_name
 
-    # Plot Pareto frontier
-    xs, ys = [], []
-    last_y = float('-inf')
-    for t in data.itertuples():
-        y = t.best_precision
-        if y > last_y:
-            last_y = y
-            xs.append(t.best_precision)
-            ys.append(t.best_queries_per_second)
-    color, linestyle, marker = linestyles[algo]
-    handle, = plt.plot(xs, ys, '-', label=algo, color=color, ms=5, mew=1, lw=2, linestyle=linestyle, marker=marker)
-    plt.gca().set_yscale('log')
-    plt.gca().set_title('Precision-Performance tradeoff - up and to the right is better')
-    plt.gca().set_ylabel('Queries per second ($s^{-1}$) - larger is better')
-    plt.gca().set_xlabel('10-NN precision - larger is better')
-    box = plt.gca().get_position()
-    # plt.gca().set_position([box.x0, box.y0, box.width * 0.8, box.height])
-    plt.gca().legend(handles, labels, loc='center left', bbox_to_anchor=(1, 0.5), prop={'size': 9})
-    plt.grid(b=True, which='major', color='0.65',linestyle='-')
-    plt.xlim([0.0, 1.03])
+#     # Plot Pareto frontier
+#     xs, ys = [], []
+#     last_y = float('-inf')
+#     for t in data.itertuples():
+#         y = t.best_precision
+#         if y > last_y:
+#             last_y = y
+#             xs.append(t.best_precision)
+#             ys.append(t.best_queries_per_second)
+#     color, linestyle, marker = linestyles[algo]
+#     handle, = plt.plot(xs, ys, '-', label=algo, color=color, ms=5, mew=1, lw=2, linestyle=linestyle, marker=marker)
+#     plt.gca().set_yscale('log')
+#     plt.gca().set_title('Precision-Performance tradeoff - up and to the right is better')
+#     plt.gca().set_ylabel('Queries per second ($s^{-1}$) - larger is better')
+#     plt.gca().set_xlabel('10-NN precision - larger is better')
+#     box = plt.gca().get_position()
+#     # plt.gca().set_position([box.x0, box.y0, box.width * 0.8, box.height])
+#     plt.gca().legend(handles, labels, loc='center left', bbox_to_anchor=(1, 0.5), prop={'size': 9})
+#     plt.grid(b=True, which='major', color='0.65',linestyle='-')
+#     plt.xlim([0.0, 1.03])
